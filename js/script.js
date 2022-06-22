84dcc6e5b4a2f4d13673ba01997bbce0e21d50b5
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-22 12:46:51 -0500

    Give camel case name to non-constant `const`

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ const projectName = 'survey-form';
 const surveyLabels = document.getElementsByTagName('label');
 
 for (const label of surveyLabels) {
-  const INPUT_TYPE = document.getElementById(label.getAttribute('for')).type;
-  if (!(INPUT_TYPE === 'radio' || INPUT_TYPE === 'checkbox')) {
+  /*
+   * Because it's value changes with each iteration, the `inputType` variable
+   * doesn't have a constant case name.
+   */
+  const inputType = document.getElementById(label.getAttribute('for')).type;
+  if (!(inputType === 'radio' || inputType === 'checkbox')) {
     label.style.margin = '1em 0';
   } else {
     label.style.margin = '0.5em 0';
